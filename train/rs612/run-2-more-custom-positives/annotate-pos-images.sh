e531c7244be198704ffceaf68f601264dd05b18c
Will Berger <will@leadoutcome.com>
2018-04-07 10:22:56 -0400

    trained haar cascade model run 2 - more custom positive images

---
+++ train/rs612/run-2-more-custom-positives/annotate-pos-images.sh
@@ -0,0 +1,1 @@
+opencv_annotation -images pos2/ -annotations pos-annotations2.txt
---
+++ train/rs612/run-2-more-custom-positives/train-cascade.sh
@@ -0,0 +1,1 @@
+opencv_traincascade -vec rs622.vec -bg neg-images.txt -numPos 8 -data model -numStages 5 -w 40 -h 40
