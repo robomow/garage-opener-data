125f7fc15aec3938f03f3da8b40f72bafff00873
Will Berger <will@leadoutcome.com>
2018-04-07 10:54:05 -0400

    trained haar cascade model run-4-color-detected

---
+++ train/rs612/run-4-color-detected/create-vec-file.sh
@@ -0,0 +1,1 @@
+opencv_createsamples -num 1900 -vec rs612.vec -info pos-annotations.txt -w 24 -h 24 
---
+++ train/rs612/run-4-color-detected/train-cascade.sh
@@ -0,0 +1,1 @@
+opencv_traincascade -vec rs612.vec -bg neg-images.txt -numPos 1400 -numNeg 1400 -data model -numStages 16 -w 24 -h 24
